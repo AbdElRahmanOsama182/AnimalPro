55a73df6174c2a06059dcb329e5e366b0326389c
ahmedyoussefg <a.goerany@gmail.com>
2024-05-19 21:35:02 +0300

    Add chameleon

--- animals.pl
+++ animals.pl
@@ -56,11 +56,14 @@ mapToCategory("has jelly", coating).
 mapToCategory("has denticles", coating).
 mapToCategory("has shell", coating).
 
-animals([animal(dog, [is_true("is omnivore"), is_true("is regular-sized"), is_true("does it run"), is_true("has fur"), is_true("is domestic"), is_true("barks")]),
+animals([
+        animal(dog, [is_true("is omnivore"), is_true("is regular-sized"), is_true("does it run"), is_true("has fur"), is_true("is domestic"), is_true("barks")]),
         animal(bat, [is_true("is omnivore"), is_true("is small"), is_true("does it fly"), is_true("has fur"), is_true("is wild"), is_true("screeches")]),
+        animal(chameleon, [is_true("is omnivore"), is_true("is small"), is_true("does it crawl"), is_true("has scales"), is_true("is wild"), is_true("hisses")]),        
         animal(cat, [is_true("is carnivore"), is_true("is small"), is_true("does it run"), is_true("has fur"), is_true("is domestic"), is_true("says meow")]),
-        animal(camel, [is_true("is herbivore"), is_true("is large"), is_true("does it run"), is_true("has fur"), is_true("is domestic"), is_true("grunts")])]).
-
+        animal(camel, [is_true("is herbivore"), is_true("is large"), is_true("does it run"), is_true("has fur"), is_true("is domestic"), is_true("grunts")])
+        ]).
+        
 animal(A) :-
     animals(Animals),
     Known0 = [],
